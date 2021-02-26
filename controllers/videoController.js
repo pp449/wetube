93bcad71881d91d64d40706d21ce6c84eba33468
pp449 <71641127+pp449@users.noreply.github.com>
2021-02-26 12:19:40 +0900

    Adding upload the video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,15 @@
-import {videos} from "../db"
-import routes from "../routes"
+import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    res.render("home", { pageTitle: "Home", videos})
+export const home = async(req, res) => {
+    try{
+        const videos = await Video.find({});
+        // console.log(videos);
+        res.render("home", { pageTitle: "Home", videos})
+    } catch(error){
+        console.log(error);
+        res.render("home", { pageTitle: "Home", videos: []})
+    }
 }
 export const search = (req, res) => {
     const {
@@ -11,12 +18,17 @@ export const search = (req, res) => {
     res.render("Search", { pageTitle: "Search", searchingBy, videos});
 }
 export const getUpload = (req, res) => res.render("Upload", {pageTitle: "Upload"});
-export const postUpload = (req,res) => {
+export const postUpload = async(req,res) => {
     const{
-        body: { file,title,description } 
+        body: { title, description},
+        file : { path }
     }= req;
-    // To Do: Upload an save video
-    res.redirect(routes.videoDetail(324393));
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description
+    });
+    res.redirect(routes.videoDetail(newVideo.id));
 }
 export const videoDetail= (req, res) => res.render("VideoDetail",  {pageTitle: "Video Detail" });
 export const editVideo= (req, res) => res.render("EditVideo", { pageTitle: "Edit Video" });
